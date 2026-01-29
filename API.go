373fdd04f45de8b7bc9f2f7c96d05c3c1f98ff14
tmorgane <morgane.tschaen@ynov.com>
2026-01-29 22:09:34 +0100

    Cache API

--- API.go
+++ API.go
@@ -3,11 +3,37 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
+	"time"
+)
+
+var (
+	cachedArtists []Artist
+	cacheMutex    sync.RWMutex
+	lastFetch     time.Time
+	cacheDuration = 10 * time.Minute
 )
 
 func fetchArtists() ([]Artist, error) {
+	cacheMutex.RLock()
+	if time.Since(lastFetch) < cacheDuration && len(cachedArtists) > 0 {
+		artists := cachedArtists
+		cacheMutex.RUnlock()
+		return artists, nil
+	}
+	cacheMutex.RUnlock()
+
+	return refreshCache()
+}
+
+func refreshCache() ([]Artist, error) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
+		cacheMutex.RLock()
+		defer cacheMutex.RUnlock()
+		if len(cachedArtists) > 0 {
+			return cachedArtists, nil
+		}
 		return nil, err
 	}
 	defer resp.Body.Close()
@@ -15,8 +41,19 @@ func fetchArtists() ([]Artist, error) {
 	var artists []Artist
 	err = json.NewDecoder(resp.Body).Decode(&artists)
 	if err != nil {
+		cacheMutex.RLock()
+		defer cacheMutex.RUnlock()
+		if len(cachedArtists) > 0 {
+			return cachedArtists, nil
+		}
 		return nil, err
 	}
+
+	cacheMutex.Lock()
+	cachedArtists = artists
+	lastFetch = time.Now()
+	cacheMutex.Unlock()
+
 	return artists, nil
 }
 
